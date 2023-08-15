4dfba44275cb124a135a4d62bac3746886647abf
Hi there, I'm Gozde <137268577+GzdeO@users.noreply.github.com>
2023-08-15 10:00:28 +0300

    Update Constants.kt

--- app/src/main/java/com/onecoder/kotlinx/utils/Constants.kt
+++ app/src/main/java/com/onecoder/kotlinx/utils/Constants.kt
@@ -1,7 +1,7 @@
 package com.onecoder.kotlinx.utils
 
 object Constants {
-    const val API_KEY="6a03b9b143e060d64d8e1e90f18f8807"
+    const val API_KEY="Enter your API_KEY pls"
     const val BASE_URL="https://api.themoviedb.org/3/"
     const val POSTER_BASEURL="https://image.tmdb.org/t/p/w500"
 }
